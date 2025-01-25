2d213041573dba9c88944e7e924759c2a65f0187
sirorezzka <145597538+sirorezzka@users.noreply.github.com>
2025-01-25 15:32:45 +0300

    возвращает новый массив и не изменяет исходный

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,8 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newArr = arr.filter(num => num >= a && num <= b);
+  return newArr;
 }
+
+let filtered = filterRange(arr, 1, 4);
