5c039a88108eea145ec4fba09438f1bcbd6b9e88
sirorezzka <145597538+sirorezzka@users.noreply.github.com>
2025-01-30 19:01:31 +0300

    скрытие текста при нажатии кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+
+  btn.addEventListener('click', function() {
+    text.hidden = !text.hidden;
+  });
+
 }
