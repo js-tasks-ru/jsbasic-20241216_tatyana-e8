518f903e5d03a82a1a4ad7823042f5d60056a9d2
sirorezzka <145597538+sirorezzka@users.noreply.github.com>
2025-01-31 19:43:39 +0300

    карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
-  // ваш код...
+  const lefttArrow = document.querySelector('.carousel__arrow_left');
+  const rightArrow = document.querySelector('.carousel__arrow_right');
+
+  const carouselInner = document.querySelector('.carousel__inner');
+  const slide = document.querySelector('.carousel__slide');
+  const sildeWidth = slide.offsetWidth;
+
+
+  let carouselInnerPosition = 0;
+
+  if (carouselInnerPosition == 0) {
+    lefttArrow.style.display = 'none';
+  }
+
+  lefttArrow.addEventListener('click', function(event) {
+
+    rightArrow.style.display = '';
+
+    carouselInnerPosition += sildeWidth;
+    carouselInner.style.transform = `translateX(${carouselInnerPosition}px)`;
+
+    if (carouselInnerPosition == 0) {
+      lefttArrow.style.display = 'none';
+    }
+
+  });
+
+  rightArrow.addEventListener('click', function(event) {
+
+    lefttArrow.style.display = '';
+
+    carouselInnerPosition -= sildeWidth;
+    carouselInner.style.transform = `translateX(${carouselInnerPosition}px)`;
+
+    if (carouselInnerPosition == -sildeWidth * (4 - 1)) {
+      rightArrow.style.display = 'none';
+    }
+
+  });
+
 }
