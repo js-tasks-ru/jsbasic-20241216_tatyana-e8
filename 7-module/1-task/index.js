f222e128ed64520605e6b19cf9c85a8a2bb5cb9e
sirorezzka <145597538+sirorezzka@users.noreply.github.com>
2025-02-09 17:50:23 +0300

    модальное окно

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,42 +1,14 @@
 import createElement from '../../assets/lib/create-element.js';
-<<<<<<< HEAD
-import escapeHtml from '../../assets/lib/escape-html.js';
-
-let categories = [
-  {
-    id: '',
-    name: 'All'
-  },
-  {
-    id: 'salads', // уникальный идентификатор категории
-    name: 'Salads' // имя категории для отображения
-  },
-  {
-    id: 'soups',
-    name: 'Soups'
-  },
-  // и другие...
-];
-
-let ribbonMenu = new RibbonMenu(categories);
-=======
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
-<<<<<<< HEAD
-=======
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
     this.render();
     this.addEventListeners();
     this.value = '';
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
   render() {
     this.elem = createElement(`
       <div class="ribbon">
@@ -49,23 +21,13 @@ export default class RibbonMenu {
         </button>
       </div>
     `);
-<<<<<<< HEAD
-=======
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
     for (let category of this.categories) {
       let categoryElem = createElement(`<a href="#" class="ribbon__item"></a>`);
       categoryElem.textContent = category.name; // insert as text, not as HTML!
       categoryElem.dataset.id = category.id;
       this.elem.querySelector('.ribbon__inner').append(categoryElem);
     }
-<<<<<<< HEAD
-    this.sub('item').classList.add('ribbon__item_active');
-  }
-  addEventListeners() {
-    this.sub('arrow_left').onclick = (event) => this.onArrowLeftClick(event);
-    this.sub('arrow_right').onclick = (event) => this.onArrowRightClick(event);
-=======
 
     this.sub('item').classList.add('ribbon__item_active');
   }
@@ -74,7 +36,6 @@ export default class RibbonMenu {
     this.sub('arrow_left').onclick = (event) => this.onArrowLeftClick(event);
     this.sub('arrow_right').onclick = (event) => this.onArrowRightClick(event);
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
     this.elem.onclick = (event) => {
       let itemElem = event.target.closest('.ribbon__item');
       if (itemElem) {
@@ -82,48 +43,32 @@ export default class RibbonMenu {
         event.preventDefault();
       }
     };
-<<<<<<< HEAD
-    this.sub('inner').onscroll = (event) => this.onScroll(event);
-  }
-=======
 
     this.sub('inner').onscroll = (event) => this.onScroll(event);
   }
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
   onArrowRightClick(event) {
     let offset = 350;
     this.sub('inner').scrollBy(offset, 0);
     this.updateArrows();
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
   onArrowLeftClick(event) {
     let offset = 350;
     this.sub('inner').scrollBy(-offset, 0);
     this.updateArrows();
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
   onItemClick(itemElem) {
     let oldActive = this.sub('item_active');
     if (oldActive) {
       oldActive.classList.remove('ribbon__item_active');
     }
-<<<<<<< HEAD
-    itemElem.classList.add('ribbon__item_active');
-    this.value = itemElem.dataset.id;
-=======
 
     itemElem.classList.add('ribbon__item_active');
 
     this.value = itemElem.dataset.id;
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
     this.elem.dispatchEvent(
       new CustomEvent('ribbon-select', {
         detail: this.value,
@@ -131,20 +76,6 @@ export default class RibbonMenu {
       })
     );
   }
-<<<<<<< HEAD
-  onScroll(event) {
-    this.updateArrows();
-  }
-  sub(ref) {
-    return this.elem.querySelector(`.ribbon__${ref}`);
-  }
-  scrollRight() {
-    return this.sub('inner').scrollWidth - (this.sub('inner').scrollLeft + this.sub('inner').clientWidth);
-  }
-  scrollLeft() {
-    return this.sub('inner').scrollLeft;
-  }
-=======
 
   onScroll(event) {
     this.updateArrows();
@@ -162,17 +93,13 @@ export default class RibbonMenu {
     return this.sub('inner').scrollLeft;
   }
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
   updateArrows() {
     if (this.scrollLeft() > 0) {
       this.sub('arrow_left').classList.add('ribbon__arrow_visible');
     } else {
       this.sub('arrow_left').classList.remove('ribbon__arrow_visible');
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
     let scrollRight = this.scrollRight();
     scrollRight = scrollRight < 1 ? 0 : scrollRight; // Это нужно для ситуации, когда скролл произошел с погрешностью
     if (scrollRight > 0) {
@@ -181,8 +108,5 @@ export default class RibbonMenu {
       this.sub('arrow_right').classList.remove('ribbon__arrow_visible');
     }
   }
-<<<<<<< HEAD
-=======
 
 }
->>>>>>> 1f7d6bd603018849e23eee888ec39f1cffafab4e
